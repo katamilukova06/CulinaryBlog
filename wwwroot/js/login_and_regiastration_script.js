269cda692c17672ffc4fbe57396d9eb63be1a711
katamilukova06 <122910788+katamilukova06@users.noreply.github.com>
2024-11-08 20:04:10 +0200

    add

---
+++ wwwroot/js/login_and_regiastration_script.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    function hiddenOpen_Closelick() {
+        let x = document.querySelector(".container-login-registration");
+        if (x.style.display == "none") {
+            x.style.display == "grid";
+        } else {
+            x.style.display == "none";
+        }
+    }
+    document.getElementById("click-to-hide").addEventListener("click", hiddenOpen_Closelick);
+    document.querySelector(".overlay").addEventListener("click", hiddenOpen_Closelick);
+});
---
+++ wwwroot/js/shared_script.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        let header = document.getElementById("header-top");
+        let scrollTop = window.scrollY;
+        let maxScroll = 250;
+
+        let opacity = Math.min(scrollTop / maxScroll, 1);
+        header.style.backgroundColor = `rgba(139, 0, 0, ${opacity})`;
+    });
+});
